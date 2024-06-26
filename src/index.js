7fd3b430a550db27982f37407a2e7cd04bbdf6f8
shankarsonar123 <65942841+shankarsonar123@users.noreply.github.com>
2024-06-26 15:46:42 +0530

    Your awesome message

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import "./index.css";
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const el = document.getElementById("root");
-const root = ReactDom.createRoot(el);
+// const el = document.getElementById("root");
+// const root = ReactDom.createRoot(el);
 
-root.render(
-    <BrowserRouter>
-    <ToastContainer
-        autoClose={1200}
-    />
-    <App/>
-    </BrowserRouter>
-
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(
+  <React.StrictMode>
+    <HashRouter>
+     <App />
+    </HashRouter>
+  </React.StrictMode>
+  );
